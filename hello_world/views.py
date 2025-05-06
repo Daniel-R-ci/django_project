b539c856312483db1d274d456bfd56321c017647
Daniel-R-ci <5zhq901m@students.codeinstitute.net>
2025-05-06 13:13:08 +0200

    Update hello_world app to differentiante between GET and POST

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method=="GET":
+        return HttpResponse(request.method)
+    elif request.method=="POST":
+        HttpResponse("You must have POSTed something!")
